e1b9eaeaf258814338abe70eb1bdc43173e98751
ImaDuhf <timperson234@gmail.com>
2023-04-05 12:39:47 +0200

    1

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import express, { request, response } from 'express';
+import express from 'express';
 import { fetchMessages, sendMessage } from '../service/messageService.js';
 
 const router = express.Router();
